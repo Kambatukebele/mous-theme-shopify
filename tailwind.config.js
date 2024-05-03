c028abbe17d6651440621c58cea3b91e1a82fcff
KAMBA  TUKEBELE <91629041+Kambatukebele@users.noreply.github.com>
2024-05-03 14:17:47 +0200

    working on favourite product section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+import { Container } from "postcss";
 import remToPxPlugin from "tailwindcss-rem-to-px";
 module.exports = {
   content: ["./**/*.{json,js,liquid}"],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+        center: true,
+      },
+    },
     extend: {},
   },
   plugins: [
